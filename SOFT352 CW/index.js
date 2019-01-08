80fa8af43231ae926c9961d9c2083e36de3585c6
LukasNgo <32539108+LukasNgo@users.noreply.github.com>
2019-01-08 00:44:50 +0000

    finished chat
    working database, fixed bugs

--- SOFT352 CW/index.js
+++ SOFT352 CW/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var socket = require("socket.io");
+var mongo = require("mongodb").MongoClient;
 
 var app = express();
 var server = app.listen(8000, function(){
@@ -13,7 +14,52 @@ var io = socket(server);
 io.on("connection", function(socket){
   console.log("made socket connection");
 
-  socket.on("chat", function(data){
-    io.sockets.emit("chat", data);
+  mongo.connect("mongodb://localhost/", function(err, client){
+    if (err) {
+      throw err;
+    }
+    console.log("made MongoDB connection");
+
+    var db = client.db("chatdb");
+
+    //get messages from database
+    db.collection("chatdb").find().limit(50).sort({_id:1}).toArray(function(err, result){
+      if (err) {
+        throw err;
+      }
+      io.sockets.emit("chatsaved", result);
+    });
+
+    socket.on("chat", function(data){
+      io.sockets.emit("chat", data);
+      db.collection("chatdb").insertOne({time:data.datetime, name: data.handle, message: data.message});
+    });
+
+
   });
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+//
+//     io.on("connection", function(socket){
+//       console.log("made socket connection");
+//
+//       socket.on("chat", function(data){
+//         chat.insert({name: data.handle, message: data.message}, function(){
+//           io.sockets.emit("chat", data);
+//         });
+//       });
+//     });
+//   });
+// });
--- SOFT352 CW/website/js/chat.js
+++ SOFT352 CW/website/js/chat.js
@@ -5,16 +5,35 @@ var handle = document.getElementById("handle");
 
 //add click event to the button and send message and handle values on click
 document.getElementById("send").addEventListener("click", function() {
-  socket.emit("chat", {
-    message:message.value,
-    handle:handle.value
-  });
+  if (handle.value == "" || message.value == "") {
+    document.getElementById("output").innerHTML += "<p>enter your name and message</p>";
+    document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight - document.getElementById("output").clientHeight;
+  }
+  else {
+    var currentdate = new Date();
+
+    socket.emit("chat", {
+      datetime:currentdate.getDate() + "/" + (currentdate.getMonth()+1) + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":"  + currentdate.getMinutes() + ":"  + currentdate.getSeconds(),
+      message:message.value,
+      handle:handle.value
+    });
+    document.getElementById("message").value = null;
+  }
+});
+
+
+socket.on("chatsaved", function(data){
+  if (data.length) {
+    document.getElementById("output").innerHTML = "";
+    for (var i = 0; i < data.length; i++) {
+      document.getElementById("output").innerHTML += "<p>[" + data[i].time + "] " + data[i].name + " : " + data[i].message + "</p>";
+    }
+    document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight - document.getElementById("output").clientHeight;
+  }
 });
 
 //when receiving data from the socket add it to the html and scroll the window to the bottom
 socket.on("chat", function(data){
-  var currentdate = new Date();
-  var datetime = currentdate.getDate() + "/" + (currentdate.getMonth()+1) + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":"  + currentdate.getMinutes() + ":"  + currentdate.getSeconds();
-  document.getElementById("output").innerHTML += "<p>[ " + datetime + "] " + data.handle + " : " + data.message + "</p>";
+  document.getElementById("output").innerHTML += "<p>[" + data.datetime + "] " + data.handle + " : " + data.message + "</p>";
   document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight - document.getElementById("output").clientHeight;
 });
